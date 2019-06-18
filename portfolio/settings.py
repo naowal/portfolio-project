90a9c5eb52a073a16a7ad3ab947134aa70b2ca96
Naowal Siripatana <way.serongang@gmail.com>
2019-06-18 15:39:32 +0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'django1234',
+        'USER':'djangodbman',
+        'PASSWORD':'IlovemyPortfo1397',
         'HOST':'localhost',
         'PORT':'5432',
     }
